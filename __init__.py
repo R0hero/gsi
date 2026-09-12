95aac1c0bef7bbbd6ba46ac70e109e4f6214f7c2
bbyel <bbyel@dtu.dk>
2026-09-12 21:44:57 -0100

    feat: add __init__.py to easily import classes

---
+++ __init__.py
@@ -0,0 +1,5 @@
+from .receiver import Receiver, Drone
+from .satellite import Satellite
+from .environment import Building, Ground
+from .ionosphere import Blob
+from .enums import ReflectionType, GroundType, AntennaType
